846c6953d43028a7cd8a01773e0d7312a23686a6
Bilge Kaan Serin <bkserin98@gmail.com>
2024-11-28 04:56:21 +0000

    Updated compute.tf and load_balancer.tf for new infrastructure setup

--- compute.tf
+++ compute.tf
@@ -1,22 +1,54 @@
 
-resource "aws_instance" "web1" {
-  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main_a.id
-  security_groups = [aws_security_group.web_sg.id]
-  key_name      = aws_key_pair.generated_key.key_name
+resource "aws_instance" "jenkins" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.jenkins_key.key_name
+  subnet_id     = module.networking.subnet_ids[0]
   tags = {
-    Name = "CCTB-ProductionEnv"
+    Name = "Jenkins"
   }
 }
 
-resource "aws_instance" "web2" {
-  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main_b.id
-  security_groups = [aws_security_group.web_sg.id]
-  key_name      = aws_key_pair.generated_key.key_name
+resource "aws_instance" "testing" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.jenkins_key.key_name
+  subnet_id     = module.networking.subnet_ids[0]
   tags = {
-    Name = "CCTB-ProductionEnv"
+    Name = "Testing"
   }
+}
+
+resource "aws_instance" "staging" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.jenkins_key.key_name
+  subnet_id     = module.networking.subnet_ids[0]
+  tags = {
+    Name = "Staging"
+  }
+}
+
+resource "aws_instance" "prod_1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.jenkins_key.key_name
+  subnet_id     = module.networking.subnet_ids[0]
+  tags = {
+    Name = "Production 1"
+  }
+}
+
+resource "aws_instance" "prod_2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.jenkins_key.key_name
+  subnet_id     = module.networking.subnet_ids[0]
+  tags = {
+    Name = "Production 2"
+  }
+}
+
+output "jenkins_instance_url" {
+  value = aws_instance.jenkins.public_ip
 }
--- load_balancer.tf
+++ load_balancer.tf
@@ -1,46 +1,47 @@
 resource "aws_lb" "web_lb" {
   name               = "web-lb"
+  resource "aws_lb" "prod_lb" {
+  name               = "prod-load-balancer"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_sg.id]
-  subnets            = [aws_subnet.main_a.id, aws_subnet.main_b.id]
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = module.networking.subnet_ids
+
+  enable_deletion_protection = false
+  enable_cross_zone_load_balancing = true
 }
 
-resource "aws_lb_target_group" "web_tg" {
-  name     = "web-tg"
+resource "aws_lb_target_group" "prod_target_group" {
+  name     = "prod-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-
-  health_check {
-    interval            = 30
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
+  vpc_id   = module.networking.vpc_id
 }
 
-resource "aws_lb_listener" "web_lb_listener" {
-  load_balancer_arn = aws_lb.web_lb.arn
+resource "aws_lb_listener" "prod_lb_listener" {
+  load_balancer_arn = aws_lb.prod_lb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web_tg.arn
+    type             = "fixed-response"
+    fixed_response {
+      status_code = 200
+      content_type = "text/plain"
+      message_body = "OK"
+    }
   }
 }
 
-resource "aws_lb_target_group_attachment" "web1" {
-  target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.web1.id
+resource "aws_lb_target_group_attachment" "prod_attachment_1" {
+  target_group_arn = aws_lb_target_group.prod_target_group.arn
+  target_id        = aws_instance.prod_1.id
   port             = 80
 }
 
-resource "aws_lb_target_group_attachment" "web2" {
-  target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.web2.id
+resource "aws_lb_target_group_attachment" "prod_attachment_2" {
+  target_group_arn = aws_lb_target_group.prod_target_group.arn
+  target_id        = aws_instance.prod_2.id
   port             = 80
 }
+
